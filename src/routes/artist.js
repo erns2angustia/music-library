7abbb6570af7a4c6358f9619d6b336bdd18a43af
Ernesto Angustia <ernesto.angustia@gmail.com>
2023-03-05 22:58:15 +0000

    Passed Deleting Artist's id

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,6 +4,10 @@ const artistController = require('../controllers/artist');
 
 const router = express.Router();
 
+router.get('/', artistController.getAllArtists);
 router.post('/', artistController.createArtist);
+router.get('/:id', artistController.getArtistById);
+router.patch('/:id', artistController.updateArtist);
+router.delete('/:id', artistController.deleteArtist);
 
 module.exports = router;
